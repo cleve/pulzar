118913ab57a8ad22ce2c88d5f01685755bddf4c6
Mauricio Cleveland <mauricio.cleveland@gmail.com>
2020-03-29 12:06:49 -0300

    Added doc string

--- app/core/core_response.py
+++ app/core/core_response.py
@@ -1,7 +1,13 @@
 class Respose:
     def __init__(self):
-        self.JSON = [('Content-Type','application/json')]
+        self.JSON = ('Content-Type','application/json')
         self.headers = []
 
     def set_headers(self, header):
+        """Add headers to the response
+        Parameters
+            header: tuple(key, value)
+        Return
+            void
+        """
         self.headers.append(header)

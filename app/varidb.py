531acb5cf75cb9d094d88627ca7140f932505eb9
Mauricio Cleveland <mauricio.cleveland@gmail.com>
2020-04-01 08:28:32 -0300

    Testing redirection

--- app/varidb.py
+++ app/varidb.py
@@ -6,5 +6,6 @@ from master.master import Master
 def application(env, start_response):
     master = Master()
     master.process_request(env, start_response)
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello World"]
+    # start_response('200 OK', [('Content-Type','text/html')])
+    start_response('302 permanent redirect', [('Content-Type','text/html'), ('Location', 'http://www.kuasard.com')])
+    return [b'']

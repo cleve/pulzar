84030cb6ebcf840bbb9ec1109700925b2d385911
Mauricio Cleveland <mauricio.cleveland@gmail.com>
2020-04-12 18:02:53 -0400

    Permanent save file sent by master

--- app/utils/file_utils.py
+++ app/utils/file_utils.py
@@ -1,4 +1,5 @@
 from utils.utils import Utils
+from utils.stream import Config
 import os
 
 
@@ -6,10 +7,19 @@ class FileUtils():
     def __init__(self, const):
         self.const = const
         self.utils = Utils()
+        self.config = Config(self.const.CONF_PATH)
         self.binary_key = b''
+        self.key = ''
+        self.volume_path = ''
+        self.init_config()
 
-    def set_key(self, binary_key):
+    def init_config(self):
+        directory = self.config.get_config('volume', 'dir')
+        self.volume_path = directory
+
+    def set_key(self, binary_key, base64_str_key):
         self.binary_key = binary_key
+        self.key = base64_str_key
 
     def file_exists(self, file_path):
         return os.path.isfile(file_path)
@@ -23,12 +33,16 @@ class FileUtils():
         except (ValueError):
             request_body_size = 0
         if request_body_size > 0:
+            destiny_path = self.volume_path + '/' + self.key
             temp_file = self.utils.get_tmp_file()
             # Read binary file sent.
             f = env[self.const.WSGI_INPUT]
             for piece in self.read_in_chunks(f):
                 temp_file.write(piece)
-            temp_file.close()
+            if destiny_path == self.utils.move_file(
+                    temp_file.name, self.volume_path + '/' + self.key):
+                temp_file.close()
+                return self.key
 
     def read_in_chunks(self, file_object, chunk_size=1024):
         """Lazy function (generator) to read a file piece by piece.

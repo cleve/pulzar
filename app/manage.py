c193925104dd1cc9698ec80a9a9f3ce5c698ce1b
Cleve <mauricio.cleveland@gmail.com>
2020-06-28 20:44:49 -0400

    Added manage app

--- app/manage.py
+++ app/manage.py
@@ -1,11 +1,40 @@
 import argparse
 
 # Internal imports
-from app.core import core_request
+from core.core_request import CoreRequest
+from utils.constants import Constants
+from utils.stream import Config
+
+
+def restore(url):
+    const = Constants()
+    config = Config(const.CONF_PATH)
+    volume_port = config.get_config('volume', 'port')
+    core_request = CoreRequest(url, volume_port, '/admin/start_backup')
+    if core_request.make_request() is not True:
+        print('Error processing the task')
+        return
+
+    print('Starting retauration...')
 
 
 def main():
-    pass
+    """Entrance point
+    """
+    arg_parse = argparse.ArgumentParser(
+        prog='manage', description='Manage DB')
+    arg_parse.add_argument(
+        '--restore',
+        metavar='volume URL',
+        action='store',
+        type=str,
+        required=True
+    )
+
+    args = arg_parse.parse_args()
+
+    if args.restore:
+        restore(args.restore)
 
 
 if __name__ == "__main__":

3f43feda6b6b38f48b44ce3ac0c2ff1f25895305
Cleve <mauricio.cleveland@gmail.com>
2020-07-03 19:14:03 -0400

    Delete watcher implemented

---
+++ app/temporal_check.py
@@ -0,0 +1,68 @@
+import requests
+
+# Internal imports
+from core.core_db import DB
+from utils.constants import Constants
+from utils.utils import Utils
+from utils.stream import Config
+
+
+class TemporalCheck:
+    """Proccess who check for files
+    that needs to be deleted
+    """
+
+    def __init__(self):
+        self.const = Constants()
+        self.utils = Utils()
+        self.temporal_files = DB(self.const.DB_NOT_PERMANENT)
+        self.master_db = DB(self.const.DB_PATH)
+        self.init_config()
+
+    def init_config(self):
+        """Getting server information
+        """
+        server_config = Config(self.const.CONF_PATH)
+        self.master_url = server_config.get_config('server', 'host')
+        self.master_port = server_config.get_config('server', 'port')
+
+    def delete_request(self, bkey):
+        key = self.utils.decode_base_64(bkey, True)
+        url = self.master_url + ':' + self.master_port
+        req = requests.delete(
+            url='http://' + url + '/delete_key/' + key
+        )
+        # Delete temporal register
+        if req.status_code >= 200 and req.status_code < 300:
+            self.temporal_files.delete_value(bkey)
+
+    def start_process(self):
+        """Review files and delete it if match with the criteria
+        """
+        current_time = self.utils.get_current_datetime()
+        with self.temporal_files.get_cursor_iterator() as txn:
+            for _, data in enumerate(txn.cursor().iternext(keys=True, values=True)):
+                bkey = data[0]
+                days = int(data[1].decode())
+                if days < 0:
+                    continue
+
+                # Get data from master
+                master_value = self.master_db.get_value(bkey, True)
+                value_datetime = self.utils.get_datetime_from_string(
+                    master_value.split(',')[1])
+                delta = (current_time - value_datetime).days
+                # Remove data
+                if delta > days:
+                    self.delete_request(bkey)
+
+
+def main():
+    """Entrance
+    """
+    temporal_checker = TemporalCheck()
+    temporal_checker.start_process()
+
+
+if __name__ == "__main__":
+    main()

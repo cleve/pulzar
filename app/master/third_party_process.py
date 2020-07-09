434ef1b269dbbebc2afcb872a1acded4e7d841bc
Cleve <mauricio.cleveland@gmail.com>
2020-07-08 21:06:50 -0400

    Fixed third party detection

--- app/master/third_party_process.py
+++ app/master/third_party_process.py
@@ -16,7 +16,7 @@ class TPProcess:
             'volume': None
         }
 
-    def process_request(self, env, start_response, url_path):
+    def process_request(self, url_path):
         # Get request type, checking for key value.
         regex_result = self.utils.get_search_regex(
             url_path, self.const.RE_THIRD_PARTY)
@@ -25,13 +25,15 @@ class TPProcess:
                 call_path_list = regex_result.groups()[0][1:].split('/')
                 file_name = call_path_list[0]
                 args = call_path_list[1:]
-                modules = self.utils.read_file_name_from_dir('/third_party/', 'py')
+                modules = self.utils.read_file_name_from_dir(
+                    'third_party/', 'py')
                 if file_name + '.py' in modules:
-                    import_fly = importlib.import_module('third_party.' + file_name)
+                    import_fly = importlib.import_module(
+                        'third_party.' + file_name)
                     j_byte = import_fly.execute(args).encode()
                     self.complex_response['action'] = self.const.TP_RESPONSE
                     self.complex_response['parameters'] = j_byte
-                
+
                 return self.complex_response
 
             except Exception as err:
--- app/third_party/search.py
+++ app/third_party/search.py
@@ -9,8 +9,9 @@ class Search:
         - dates
     """
 
-    def __init__(self, arg1):
+    def __init__(self, arg1, arg2):
         self.arg1 = arg1
+        self.arg2 = arg2
 
     def do_the_work(self):
         """Method who actually do the search
@@ -20,13 +21,13 @@ class Search:
     def json_return(self):
         """Json converter
         """
-        return json.dumps({'my_arg': self.arg1})
+        return json.dumps({'my_args': [self.arg1, self.arg2]})
 
 
 def execute(arguments):
     """Entrance point
     """
     # Check arguments
-    search = Search(arguments[0])
+    search = Search(arguments[0], arguments[1])
     search.do_the_work()
     return search.json_return()

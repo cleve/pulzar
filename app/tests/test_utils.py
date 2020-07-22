19f17b41b8f34d46d50de5c794ad519816cedb84
Cleve <mauricio.cleveland@gmail.com>
2020-07-21 20:08:20 -0400

    Unittest

--- app/tests/test_utils.py
+++ app/tests/test_utils.py
@@ -1,10 +1,35 @@
 import unittest
 from utils.utils import Utils
 
-class TestUtilsMethods(unittest.TestCase):
 
-    def test_base64(self):
-        pass
+class TestUtilsMethods(unittest.TestCase):
+    """Testing Util class
+    """
+
+    def setUp(self):
+        self.utils = Utils()
+
+    def test_encode_base64(self):
+        self.assertEqual(self.utils.encode_base_64(
+            'string_to_encode'), b'c3RyaW5nX3RvX2VuY29kZQ==')
+
+        self.assertEqual(self.utils.encode_base_64(
+            'string_to_encode', to_str=True), 'c3RyaW5nX3RvX2VuY29kZQ==')
+
+    def test_decode_base64(self):
+        self.assertEqual(self.utils.decode_base_64(
+            b'c3RyaW5nX3RvX2VuY29kZQ=='), b'string_to_encode')
+
+        self.assertEqual(self.utils.decode_base_64(
+            b'c3RyaW5nX3RvX2VuY29kZQ==', to_str=True), 'string_to_encode')
+
+    def test_encode_byte_base_64(self):
+        self.assertEqual(self.utils.encode_byte_base_64(
+            b'string_to_encode'), b'c3RyaW5nX3RvX2VuY29kZQ==')
+
+        self.assertEqual(self.utils.encode_byte_base_64(
+            b'string_to_encode', to_str=True), 'c3RyaW5nX3RvX2VuY29kZQ==')
+
 
 if __name__ == '__main__':
     unittest.main()

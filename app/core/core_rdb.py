21e2419d3d015f7288effb125ffafa692d0c2897
Cleve <mauricio.cleveland@gmail.com>
2020-07-26 18:06:55 -0400

    Added rdb helper

--- app/core/core_rdb.py
+++ app/core/core_rdb.py
@@ -5,5 +5,13 @@ class RDB:
     """Relational DB package
     """
 
-    def __init__(self, *args, **kwargs):
-        self.db = None
+    def __init__(self, db_path):
+        self.database = sqlite3.connect(db_path)
+        self.cursor = self.database.cursor()
+
+    def execute_sql(self, query):
+        self.cursor.execute(query)
+        self.cursor.commit()
+
+    def close(self):
+        self.cursor.close()

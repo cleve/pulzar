094812ffd69bef03bc95070fab06301fe12f77cc
Cleve <mauricio.cleveland@gmail.com>
2020-07-28 21:44:18 -0400

    Handled json body

--- app/core/core_body.py
+++ app/core/core_body.py
@@ -1,13 +1,17 @@
 from urllib.parse import parse_qs
 from html import escape
 from utils.constants import Constants
+from utils.utils import Utils
 
 
 class Body:
     def __init__(self):
         self.const = Constants()
+        self.utils = Utils()
 
     def extract_params(self, env):
+        """Extracting body paremeters
+        """
         # the environment variable CONTENT_LENGTH may be empty or missing
         try:
             request_body_size = int(env[self.const.CONTENT_LENGTH])
@@ -16,6 +20,9 @@ class Body:
         if request_body_size == 0:
             return None
         request_body = env[self.const.WSGI_INPUT].read(request_body_size)
+        # JSON type
+        if env[self.const.CONTENT_TYPE] == self.const.JSON_REQUEST:
+            return self.utils.json_to_py(request_body)
         return parse_qs(request_body)
 
     def extract_binary(self, env):

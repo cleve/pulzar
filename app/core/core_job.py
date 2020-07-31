b7316066aaee717a0b14ace92e0d6fa9c753c798
Cleve <mauricio.cleveland@gmail.com>
2020-07-31 18:50:30 -0400

    Registering job on master fixed

--- app/core/core_job.py
+++ app/core/core_job.py
@@ -1,9 +1,8 @@
-# Internal imports
 from core.core_request import CoreRequest
+from core.core_rdb import RDB
 from utils.node_utils import NodeUtils
 from utils.constants import ReqType
 from utils.utils import Utils
-from core.core_rdb import RDB
 
 
 class Job:
@@ -13,20 +12,33 @@ class Job:
     def __init__(self, job_params):
         self.job_params = job_params
         self.utils = Utils()
+        self.job_id = None
 
-    def register_job(self, path_db_jobs):
+    def unregister_job(self, path_db_jobs):
+        """Mark as failed job in master
+        """
+        print('uregistering job')
+        # Master job database
+        data_base = RDB(path_db_jobs)
+        sql = 'UPDATE job SET ready = 2 WHERE id = {}'.format(self.job_id)
+        data_base.execute_sql(
+            sql
+        )
+
+    def register_job(self, path_db_jobs, node):
         """Register job in master
         """
         print('registering job')
-        job_path = self.job_params['job_path'] + self.job_params['job_name']
+        job_path = self.job_params['job_path']
+        job_name = self.job_params['job_name']
         parameters = self.utils.py_to_json(self.job_params['parameters'])
         # Master job database
         data_base = RDB(path_db_jobs)
-        sql = 'INSERT INTO job (job_name, parameters, creation_time, ready) values (?, ?, ?, ?)'
-        data_base.execute_sql_with_params(
+        sql = 'INSERT INTO job (job_name, job_path, parameters, node, creation_time, ready) values (?, ?, ?, ?, ?, ?)'
+        self.job_id = data_base.execute_sql_insert(
             sql,
             (
-                job_path, parameters, self.utils.get_current_datetime(), 0
+                job_name, job_path, parameters, node, self.utils.get_current_datetime(), 0
             )
         )
 
@@ -39,12 +51,16 @@ class Job:
 
         node_utils = NodeUtils(const)
         node = node_utils.pick_a_volume()
-        print('Sending job')
+        if node is None:
+            return False
+        print('Sending job to node ', node)
+        # Register in data base
+        self.register_job(const.DB_JOBS, node.decode())
         request = CoreRequest(node.decode(), '9001', '/send_job')
         request.set_type(ReqType.POST)
         request.set_payload(self.job_params)
         job_response = request.make_request(json_request=True)
-        if job_response:
-            # Register in data base
-            self.register_job(const.DB_JOBS)
+        if not job_response:
+            # removing job
+            self.unregister_job(const.DB_JOBS)
         return job_response

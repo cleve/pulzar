61b824df134869e350ae1a8ed6c028de3c3109a9
Cleve <mauricio.cleveland@gmail.com>
2020-08-02 17:07:45 -0400

    Using importlib

--- app/launch_jobs.py
+++ app/launch_jobs.py
@@ -1,3 +1,5 @@
+import importlib
+import os
 from pulzarutils.utils import Utils
 from pulzarutils.constants import Constants
 from pulzarcore.core_rdb import RDB
@@ -13,6 +15,20 @@ class LaunchJobs:
         self.data_base = RDB(self.const.DB_NODE_JOBS)
         self.jobs_to_launch = []
 
+    def process_params(self):
+        for job in self.jobs_to_launch:
+            args = job['job_args']
+            if args != '':
+                job['job_args'] = self.utils.json_to_py(args)
+
+    def execute_jobs(self):
+        for job in self.jobs_to_launch:
+            print('Launching', job['job_id'], 'located in ', job['job_path'])
+            custom_module = os.path.splitext(job['job_path'])[
+                0].replace('/', '.')
+            print(custom_module)
+            import_fly = importlib.import_module(custom_module)
+            j_byte = import_fly.execute(['222'])
 
     def search_jobs(self):
         """Search job scheduled
@@ -26,10 +42,8 @@ class LaunchJobs:
                 'job_args': row[2],
                 'job_creation': row[4]
             })
-        
+
 
 launcher = LaunchJobs()
 launcher.search_jobs()
-
-
-
+launcher.execute_jobs()

4d7fa3e42fee4bf602bc25770986f365a33fb143
Cleve <mauricio.cleveland@gmail.com>
2020-08-01 20:41:02 -0400

    New class to handle jobs

--- app/launch_jobs.py
+++ app/launch_jobs.py
@@ -1,2 +1,18 @@
+from utils.utils import Utils
+from utils.constants import Constants
+from core.core_rdb import RDB
+
+
 class LaunchJobs:
-    pass
+    """Handle job scheduled
+    """
+
+    def __init__(self):
+        self.const = Constants()
+        self.utils = Utils()
+        self.data_base = RDB(self.const.DB_NODE_JOBS)
+
+    def search_jobs(self):
+        """Search job scheduled
+        """
+        pass

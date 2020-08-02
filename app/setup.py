7d7bd90186a1be60a1e2ed1919943a95e8804d02
Cleve <mauricio.cleveland@gmail.com>
2020-08-01 20:42:22 -0400

    Creation of instalator

---
+++ app/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pulzar',
+      version='0.1',
+      description='Distributed database and jobs',
+      url='http://github.com/cleve/pulzar',
+      author='Mauricio Cleveland',
+      author_email='mauricio.cleveland@gmail.com',
+      license='Apache',
+      packages=['core'],
+      zip_safe=False)

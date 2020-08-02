b8da20a28c490d872d76fc49a5f23a3cab8e1e23
Cleve <mauricio.cleveland@gmail.com>
2020-08-02 13:03:35 -0400

    Extracting data from jobs

--- app/launch_jobs.py
+++ app/launch_jobs.py
@@ -11,8 +11,25 @@ class LaunchJobs:
         self.const = Constants()
         self.utils = Utils()
         self.data_base = RDB(self.const.DB_NODE_JOBS)
+        self.jobs_to_launch = []
+
 
     def search_jobs(self):
         """Search job scheduled
         """
-        pass
+        sql = 'SELECT * FROM job WHERE ready = 0'
+        rows = self.data_base.execute_sql_with_results(sql)
+        for row in rows:
+            self.jobs_to_launch.append({
+                'job_id': row[0],
+                'job_path': row[1],
+                'job_args': row[2],
+                'job_creation': row[4]
+            })
+        
+
+launcher = LaunchJobs()
+launcher.search_jobs()
+
+
+

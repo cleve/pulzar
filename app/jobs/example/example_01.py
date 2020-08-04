db78408abbbeae6d827ff707fea7c6736b9a65cb
Cleve <mauricio.cleveland@gmail.com>
2020-08-04 19:21:52 -0400

    Clean implementation

--- app/jobs/example/example_01.py
+++ app/jobs/example/example_01.py
@@ -9,22 +9,16 @@ class Example(CoreJobs):
     def __init__(self, params):
         super().__init__(params)
 
-    def run_job(self):
-        try:
-            time.sleep(2)
-            r = 1/0
-            return True
-        except Exception as err:
-            self.mark_as_failed()
-            self.add_log(str(err))
-
-        return False
+    @CoreJobs._pulzar_run_job
+    def run_my_code(self):
+        time.sleep(20)
+        r = 1.0/10.0
+        self.pulzar_add_log('Result is: ' + str(r))
 
 
 def execute(arguments):
     """Point of entrance
     """
     example = Example(arguments)
-    example.run_job()
-    example.notification()
+    example.run_my_code()
     return example.is_the_job_ok()

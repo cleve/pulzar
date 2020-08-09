c6ffb814ab7778e82894b5efdb3aef189c9a8041
Cleve <mauricio.cleveland@gmail.com>
2020-08-09 11:26:20 -0400

    Fixed encode issue with composed key

--- app/master/get_process.py
+++ app/master/get_process.py
@@ -19,20 +19,22 @@ class GetProcess:
         # Get request type, checking for key value.
         regex_result = self.utils.get_search_regex(
             url_path, self.const.RE_GET_VALUE)
+
         if regex_result:
             try:
                 key_to_search = regex_result.groups()[0]
                 # Searching in the database
                 key_to_binary = self.utils.encode_base_64(key_to_search)
                 # Getting volume,string_datetime
-                composed_value = self.db_values.get_value(key_to_binary)
+                composed_value = self.db_values.get_value(
+                    key_to_binary, to_str=True)
                 if composed_value is None:
                     self.complex_response['action'] = self.const.KEY_NOT_FOUND
                     return self.complex_response
                 # Extracting data.
                 volume = composed_value.split(',')[0]
                 self.complex_response['action'] = self.const.KEY_FOUND
-                self.complex_response['volume'] = volume
+                self.complex_response['volume'] = volume.encode()
                 return self.complex_response
 
             except Exception as err:

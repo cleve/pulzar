f59dd8ec5c1a5b59cafd22f19f9f13a6df4de627
Cleve <mauricio.cleveland@gmail.com>
2020-08-09 11:26:33 -0400

    Dot support for volume

--- app/pulzarutils/constants.py
+++ app/pulzarutils/constants.py
@@ -69,9 +69,8 @@ class Constants:
         self.RE_URL_OPTION_ORDER = r'([\w]+)*\/([\w]+)+$'
 
         # API calls
-        self.RE_GET_VALUE = r'/get_key/(\w+)$'
-        self.RE_DELETE_VALUE = r'/delete_key/(\w+)$'
-        self.RE_POST_VALUE = r'/add_key/(\w+)$'
+        self.RE_GET_VALUE = r'\/get_key\/([\w|\.]+)$'
+        self.RE_DELETE_VALUE = r'\/delete_key\/([\w|\.]+)$'
         self.RE_PUT_VALUE = r'\/add_key\/([\w|\.]+)$'
         self.RE_THIRD_PARTY = r'/third_party((/\w+)+)'
         self.RE_LAUNCH_JOB = r'\/launch_job([\/\w\d]+)\/([\w\d]+)'

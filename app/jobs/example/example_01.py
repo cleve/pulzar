658db27bce395cc35d618c2d8a2e89eaf8d60352
Cleve <mauricio.cleveland@gmail.com>
2020-08-09 17:58:56 -0400

    Testing data usage

--- app/jobs/example/example_01.py
+++ app/jobs/example/example_01.py
@@ -9,12 +9,23 @@ class Example(CoreJobs):
     def __init__(self, params):
         super().__init__(params)
 
+    def process_data(self):
+        if self.pulzar_get_filepath() is not None:
+            print('Opening ', self.pulzar_get_filepath())
+            line_number = 1
+            with open(self.pulzar_get_filepath(), 'rb') as f:
+                for line in f:
+                    print('(' + str(line_number) + ')', line)
+                    line_number += 1
+
     @CoreJobs._pulzar_run_job
     def run_my_code(self):
         print('Parameters: ', self.parameters)
         number = self.parameters['arg1']
         number_two = self.parameters['arg2']
-        time.sleep(1)
+
+        self.process_data()
+
         r = number / number_two
         self.pulzar_add_log('Result is: ' + str(r))
 

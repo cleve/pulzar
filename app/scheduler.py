c3b9befd296b72952637544e67887175df7112e2
Cleve <mauricio.cleveland@gmail.com>
2020-08-14 21:11:19 -0400

    Implementation of time intervals

--- app/scheduler.py
+++ app/scheduler.py
@@ -7,7 +7,6 @@ from pulzarutils.constants import Constants
 from pulzarutils.constants import ReqType
 from pulzarutils.node_utils import NodeUtils
 from pulzarcore.core_rdb import RDB
-from pulzarcore.core_db import DB
 from pulzarcore.core_request import CoreRequest
 from pulzarcore.core_job_master import Job
 
@@ -36,7 +35,6 @@ class Scheduler():
             limit)
         rows = self.schedule_data_base.execute_sql_with_results(sql)
         for row in rows:
-            print(row[3], type(row[3]))
             self.jobs_to_launch.append({
                 'job_id': row[0],
                 'job_name': row[1],
@@ -52,25 +50,68 @@ class Scheduler():
         """
         job_object = Job(params)
         if job_object.send_scheduled_job(self.const, params):
+            # Update next iteration time
+            self.update_next_execution_job(params['job_id'])
             return True
+
         return False
+
+    def schedule_details(self, job_params, interval, time_unit):
+        """Since parameter, schedule according it
+            return schedule object
+        """
+        schedule_object = None
+        if interval == 'minutes':
+            schedule_object = schedule.every(int(time_unit)).minutes.do(
+                self._notify_to_node, params=job_params)
+
+        elif interval == 'hours':
+            schedule_object = schedule.every(int(time_unit)).hours.do(
+                self._notify_to_node, params=job_params)
+
+        elif interval == 'days':
+            schedule_object = schedule.every(int(time_unit)).days.do(
+                self._notify_to_node, params=job_params)
+
+        elif interval == 'weeks':
+            schedule_object = schedule.every(int(time_unit)).weeks.do(
+                self._notify_to_node, params=job_params)
+
+        return schedule_object
+
+    def update_next_execution_job(self, job_id):
+        """Update the date of the next execution
+        """
+        for job in schedule.jobs:
+            if job_id in job.tags:
+                sql = 'UPDATE schedule_job SET next_execution = ? WHERE job_id = {}'.format(
+                    job_id)
+                self.schedule_data_base.execute_sql_update(
+                    sql, (job.next_run,))
 
     def _schedule_jobs(self):
         """Execute jobs selected
         """
-        print('Schedule jobs', schedule.jobs)
         for iter_job in range(len(self.jobs_to_launch)):
             try:
                 job = self.jobs_to_launch.pop()
                 print('Scheduling', job['job_id'],
                       'located in ', job['job_path'])
                 job['scheduled'] = 1
-                schedule.every(1).minutes.do(self._notify_to_node, params=job)
+                # Getting configuration for schedule job.
+                interval = job['job_interval']
+                time_unit = job['job_time_unit']
+                new_object = self.schedule_details(job, interval, time_unit)
+                if new_object is None:
+                    continue
+                # Tagging the job with the ID
+                new_object = new_object.tag(job['job_id'])
+                next_execution = new_object.next_run
                 # mark as scheduled
-                sql = 'UPDATE schedule_job SET scheduled = 1 WHERE job_id = {}'.format(
+                sql = 'UPDATE schedule_job SET scheduled = 1 , next_execution = ? WHERE job_id = {}'.format(
                     job['job_id'])
-                print(sql)
-                self.schedule_data_base.execute_sql(sql)
+                self.schedule_data_base.execute_sql_update(
+                    sql, (next_execution,))
             except Exception as err:
                 print('Error executing the job: ', err)
 
@@ -80,7 +121,7 @@ class Scheduler():
             self._process_params()
             self._schedule_jobs()
             schedule.run_pending()
-            time.sleep(1)
+            time.sleep(20)
 
 
 scheduler_object = Scheduler()

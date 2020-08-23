626df28d30851e622dbd63803d7e0ffb58f4e3f8
Cleve <mauricio.cleveland@gmail.com>
2020-08-22 21:15:25 -0400

    Added property

--- app/pulzarutils/http_response.py
+++ app/pulzarutils/http_response.py
@@ -7,11 +7,25 @@ class HttpResponse:
 
     def __init__(self):
         self.utils = Utils()
+        self.volume = None
+        self.code_type = 'default'
+        self.http_code = '200 OK'
         self.response = {
             'response': None,
             'status': 'ok',
             'msg': ''
         }
+
+    @property.setter
+    def set_message(self, message):
+        """Add message into the dictionary
+        """
+        self.response['msg'] = message
+
+    def mark_as_failed(self):
+        """Set the response as failed one
+        """
+        self.response['status'] = 'ko'
 
     def set_response(self, response):
         """Add response into the dictionary

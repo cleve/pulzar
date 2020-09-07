c5c82aeef70414da4886c62b6342784c43697d82
Cleve <mauricio.cleveland@gmail.com>
2020-09-07 20:24:18 -0300

    Added new job with submodules

---
+++ app/jobs/complex_example/core/database.py
@@ -0,0 +1,9 @@
+import sys
+
+
+class DB:
+    def __init__(self):
+        self.cursor = None
+
+    def execute_query(self, query):
+        return query
--- app/jobs/complex_example/example_01.py
+++ app/jobs/complex_example/example_01.py
@@ -1,4 +1,6 @@
 from pulzarcore.core_jobs import CoreJobs
+# Internal import
+from .core.database import DB
 
 
 class Example(CoreJobs):
@@ -8,11 +10,19 @@ class Example(CoreJobs):
 
     def __init__(self, params):
         super().__init__(params)
+        self.database = DB()
+
+    @CoreJobs._pulzar_run_job
+    def run_my_code(self):
+        print('Parameters: ', self.parameters)
+        query = self.database.execute_query('SELECT * FROM DB')
+        self.pulzar_add_log(query)
+        self.pulzar_set_output('Result is: ' + query)
 
 
 def execute(arguments):
     """Point of entrance
     """
     example = Example(arguments)
-    # example.run_my_code()
+    example.run_my_code()
     return example.is_the_job_ok()

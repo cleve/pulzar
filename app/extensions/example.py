37357971dba3637918f1c6f8f2e1d7042478af65
Cleve <mauricio.cleveland@gmail.com>
2020-09-12 21:37:24 -0300

    Updating method

--- app/extensions/example.py
+++ app/extensions/example.py
@@ -1,16 +1,15 @@
-import json
-
 class Example:
     def __init__(self, arg1):
         self.arg1 = arg1
-    
+
     def hello(self):
         print('Hello example with arg ', self.arg1)
-    
-    def json_return(self):
-        return json.dumps({'my_arg': self.arg1})
 
-def execute(arguments):
+    def method_return(self):
+        return {'my_arg': self.arg1}
+
+
+def execute(arguments, params):
     example = Example(arguments[0])
     example.hello()
-    return example.json_return()
+    return example.method_return()

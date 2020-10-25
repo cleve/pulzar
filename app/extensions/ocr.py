ca932e8ce3d2431ce040974ba4b7baf07817007f
Cleve <mauricio.cleveland@gmail.com>
2020-10-25 11:49:14 -0300

    OCR implementation complete

--- app/extensions/ocr.py
+++ app/extensions/ocr.py
@@ -17,12 +17,14 @@ class Ocr:
         This class allow us to get and search by text
 
         Use:
-            server/extension/ocr?search=text
+            server/extension/ocr/[image_name]?search=text&invert=[0|1]
     """
 
-    def __init__(self, arg1, search, file_path):
+    def __init__(self, arg1, search, invert, percent, file_path):
         self.arg1 = arg1
         self.search = search
+        self.invert = int(invert)
+        self.percent = percent
         self.file_path = file_path
         self.public = Public()
         self.response = {}
@@ -31,8 +33,16 @@ class Ocr:
         """Method who actually do the hard work
         """
         image = Image.open(self.file_path)
+        # Invert image if argument is not zero
+        if self.invert > 0:
+            image = ImageOps.invert(image.convert('RGB'))
         text = pytesseract.image_to_string(image, timeout=10)
         self.response['text'] = text
+        if self.search is not None:
+            if text.upper().find(self.search.upper()) >= 0:
+                self.response['contains'] = True
+            else:
+                self.response['contains'] = False
 
     def get_response(self):
         """Get the final object
@@ -44,33 +54,34 @@ def execute(arguments, params, file_path=None):
     """Entrance point
     """
     search = None
-    percent = None
-    invert = None
+    percent = 80
+    invert = 0
     # Check arguments
     if file_path is None:
         raise ValueError('file path empty')
+    # Check params
     if params:
         try:
             # Search text
             if 'search' in params:
                 search = params['search'][0]
                 if not isinstance(search, str):
-                    search = None
+                    raise Exception('search must be string')
             # Percent
             if 'percent' in params:
                 percent = params['percent'][0]
                 if not isinstance(percent, int):
-                    percent = None
+                    raise Exception('percent must be integer')
             # Invert
             if 'invert' in params:
                 invert = params['invert'][0]
                 if not isinstance(invert, int):
-                    invert = None
+                    raise Exception('invert must be integer')
         except Exception as err:
             print(err)
 
     if len(arguments) == 0:
-        return json.dumps({'err': 'no args detected'})
-    ocr = Ocr(arguments[0], search, file_path)
+        raise Exception('no args detected')
+    ocr = Ocr(arguments[0], search, invert, percent, file_path)
     ocr.do_the_work()
     return ocr.get_response()

cf825716dff763c20189928646bd63e3b194faf4
Cleve <mauricio.cleveland@gmail.com>
2020-12-07 15:50:39 -0300

    Docstring added

--- app/extensions/example.py
+++ app/extensions/example.py
@@ -3,6 +3,18 @@ from pulzarutils.extension import Extension
 
 class Example(Extension):
     def __init__(self, arguments, params, file_path=None):
+        '''Receiving values
+            URL: http://master:[port]/extension/arg_1/arg_2/arg_n?param_1=1&param_2=2&param_n=n
+
+        arguments
+        ---------
+        arguments = ['arg_1', 'arg_2', 'arg_n']
+
+        parameters
+        ----------
+        params = {'param_1': [1], 'param_2': [2], 'param_n': [n]}
+        '''
+
         self.args = arguments
         self.params = params
 
@@ -14,5 +26,7 @@ class Example(Extension):
         return {'my_arg': self.args, 'my_params': self.params}
 
     def execute(self):
+        '''Mandatory name
+        '''
         self.hello()
         return self.method_return()

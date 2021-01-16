ed2d6691a6d508762453e56d60348c5238526703
Mauricio <mauricio.cleveland@gmail.com>
2021-01-16 14:27:40 -0300

    Thread pool implemetation completed

--- app/test_integration/test_stress.py
+++ app/test_integration/test_stress.py
@@ -5,13 +5,18 @@ import string
 from timeit import default_timer as timer
 from concurrent.futures import ThreadPoolExecutor
 
-TEST_CASES = 10
+TEST_CASES = 10000
 THREAD = 1
 MASTER_URL = 'http://127.0.0.1:31414/'
 VOLUME_URL = 'http://127.0.0.1:31415/'
 
-thread_pool = ThreadPoolExecutor(max_workers=5)
+thread_pool_write = ThreadPoolExecutor(max_workers=5)
+thread_pool_read = ThreadPoolExecutor(max_workers=5)
+thread_pool_delete = ThreadPoolExecutor(max_workers=5)
 keys = []
+time_request = []
+time_register = []
+time_errors = []
 
 # Binary files to upload tests
 binary_data = {
@@ -24,6 +29,17 @@ binary_data = {
 def get_random_text(string_lenght):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=string_lenght))
 
+def evaluate_time(report_name):
+    if len(time_request) > 0:
+        print('{}: Good cases {} request time with average of {}'.format(
+            report_name, len(time_request), sum(time_request)/len(time_request)))
+        print('{}: Good cases {} total time with average of {}'.format(
+            report_name, len(time_register), sum(time_register)/len(time_register)))
+
+    if len(time_errors) > 0:
+        print('{}: Bad cases {} total time with average of {}'.format(
+            report_name, len(time_errors), sum(time_errors)/len(time_errors)))
+
 def futures_write():
     results = []
     for _ in range(TEST_CASES):
@@ -34,19 +50,27 @@ def futures_write():
         url = MASTER_URL + 'add_key/test_data/' + key + dict_key + '?temporal=1'
         if dict_key == '_text_file.txt':
             # File creation or binary selection
-            thread_pool.submit(write_test, url, results, dict_key, key, 'File')
+            thread_pool_write.submit(write_test, url, results, dict_key, key, 'File')
             
         else:
-            thread_pool.submit(write_test, url, results, dict_key, key, 'Regular')
-        
-    thread_pool.shutdown(wait=True)
+            thread_pool_write.submit(write_test, url, results, dict_key, key, 'Regular')
+
+    thread_pool_write.shutdown(wait=True)    
     return results
 
+def futures_delete(results):
+    for key in results:
+        url = MASTER_URL + 'delete_key/test_data/' + key
+        thread_pool_delete.submit(delete_test, url)
+    thread_pool_delete.shutdown(wait=True)
+
+def futures_read(results):
+    for key in results:
+        url = MASTER_URL + 'get_key/test_data/' + key
+        thread_pool_read.submit(read_test, url)
+    thread_pool_read.shutdown(wait=True)
+
 def write_test(url, result_list, dict_key, key, test_type='File'):
-    print('====== Writing tests ======')
-    time_request = []
-    time_register = []
-    time_errors = []
     if test_type == 'File':
         # File creation or binary selection
         with tempfile.TemporaryFile() as tmp:
@@ -72,71 +96,36 @@ def write_test(url, result_list, dict_key, key, test_type='File'):
     else:
         time_errors.append(timer() - start)
 
-    if len(time_request) > 0:
-        print('Good cases {} request time with average of {}'.format(
-            len(time_request), sum(time_request)/len(time_request)))
-        print('Good cases {} total time with average of {}'.format(
-            len(time_register), sum(time_register)/len(time_register)))
-
-    if len(time_errors) > 0:
-        print('Bad cases {} total time with average of {}'.format(
-            len(time_errors), sum(time_errors)/len(time_errors)))
-
-
-def start_read_test():
-    print('====== Read tests ======')
+def read_test(url):
     time_request = []
     time_register = []
     time_errors = []
-    for key in keys:
-        start = timer()
-        req = requests.get(
-            url=MASTER_URL + 'get_key/mcleveland_test_data/' + key
-        )
-        if req.status_code >= 200 and req.status_code < 300:
-            time_register.append(timer() - start)
-            time_request.append(req.elapsed.total_seconds())
-        else:
-            time_errors.append(timer() - start)
+    start = timer()
+    req = requests.get(url=url)
+    if req.status_code >= 200 and req.status_code < 300:
+        time_register.append(timer() - start)
+        time_request.append(req.elapsed.total_seconds())
+    else:
+        time_errors.append(timer() - start)
 
-    if len(time_request) > 0:
-        print('Good cases {} request time with average of {}'.format(
-            len(time_request), sum(time_request)/len(time_request)))
-        print('Good cases {} total time with average of {}'.format(
-            len(time_register), sum(time_register)/len(time_register)))
-
-    if len(time_errors) > 0:
-        print('Bad cases {} total time with average of {}'.format(
-            len(time_errors), sum(time_errors)/len(time_errors)))
-
-
-def start_delete_test():
-    print('====== Delete tests ======')
+def delete_test(url):
     time_request = []
     time_register = []
     time_errors = []
-    for key in keys:
-        start = timer()
-        req = requests.delete(
-            url=MASTER_URL + 'delete_key/mcleveland_test_data/' + key
-        )
-        if req.status_code >= 200 and req.status_code < 300:
-            time_register.append(timer() - start)
-            time_request.append(req.elapsed.total_seconds())
-        else:
-            time_errors.append(timer() - start)
+    start = timer()
+    req = requests.delete(url=url)
+    if req.status_code >= 200 and req.status_code < 300:
+        time_register.append(timer() - start)
+        time_request.append(req.elapsed.total_seconds())
+    else:
+        time_errors.append(timer() - start)
 
-    if len(time_request) > 0:
-        print('Good cases {} request time with average of {}'.format(
-            len(time_request), sum(time_request)/len(time_request)))
-        print('Good cases {} total time with average of {}'.format(
-            len(time_register), sum(time_register)/len(time_register)))
+results = futures_write()
+evaluate_time('Write')
+futures_read(results)
+evaluate_time('Read')
+futures_delete(results)
+evaluate_time('Delete')
 
-    if len(time_errors) > 0:
-        print('Bad cases {} total time with average of {}'.format(
-            len(time_errors), sum(time_errors)/len(time_errors)))
-
-
-futures_write()
 # start_read_test()
 # start_delete_test()

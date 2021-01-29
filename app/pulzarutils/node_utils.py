70689b9e3d121fbcbb2f92480a05f5c5867f143d
Mauricio <mauricio.cleveland@gmail.com>
2021-01-29 19:54:59 -0300

    Refactor of algorithm to select node

--- app/pulzarutils/node_utils.py
+++ app/pulzarutils/node_utils.py
@@ -80,7 +80,7 @@ class NodeUtils:
         Return
         ------
         list
-            list of tuples with nodes
+            list of nodes
         '''
         job_path = job_path[1:]
         query = f'''SELECT
@@ -90,24 +90,28 @@ class NodeUtils:
         LEFT JOIN job_catalog_node_register ON job_catalog.id = job_catalog_node_register.job_catalog_id
         WHERE job_catalog.path = "{job_path}";
         '''
-        print(query)
-        return self.job_db.execute_sql_with_results(query)
+        list_of_tuples = self.job_db.execute_sql_with_results(query)
+        return [node[0].encode() for node in list_of_tuples]
     
     def pick_a_volume2(self, job_path):
-        """Volume selection using the load and the job
+        """Volume selection using the workload and the job
         
+        Parameter
+        ---------
+        job_path : str
+            String with the path to the job
+
         Return
         ------
         byte
             URL without port
         """
+        nodes_online = []
         volumes = self.db_volumes.get_keys_values()
-        print('=====> ', self._node_candidates_since_path(job_path))
+        node_candidates = self._node_candidates_since_path(job_path)
         current_datetime = self.utils.get_current_datetime()
-        min_value = 100
-        server = None
+        # Filtering by online nodes
         for elem in volumes:
-            # meta_raw[0] = percent, meta_raw[1] = load
             meta_raw = self.utils.decode_byte_to_str(elem[1]).split(':')
             percent = int(meta_raw[0])
             last_update_reported = self.utils.get_datetime_from_string(
@@ -116,7 +120,15 @@ class NodeUtils:
             # Check availability of node.
             if delta_time.total_seconds() >= self.second_range:
                 continue
-            if percent < min_value:
-                min_value = percent
-                server = elem[0]
-        return server
+            # Online nodes with their workload
+            nodes_online.append((elem[0], percent))
+        
+        # Ordering to easy selection
+        nodes_online = sorted(nodes_online, key=lambda x : x[1])
+        nodes_online = [elem[0] for elem in nodes_online]
+        while len(nodes_online) > 0:
+            random_node = self.utils.get_random_element_from_list(nodes_online)
+            nodes_online.remove(random_node)
+            if random_node in node_candidates:
+                return random_node 
+        return None

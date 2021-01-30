898d96ac9161cd00cdb9388284b48fe7240e1765
Mauricio <mauricio.cleveland@gmail.com>
2021-01-30 20:55:37 -0300

    Better unittest

--- app/tests/test_node_utils.py
+++ app/tests/test_node_utils.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 from unittest.mock import MagicMock
 import math
+import datetime
 from pulzarutils.utils import Constants
 from pulzarutils.utils import Utils
 from pulzarutils.node_utils import NodeUtils
@@ -14,12 +15,35 @@ class TestUtilsMethods(unittest.TestCase):
         const = Constants()
         self.node_utils = NodeUtils(const)
 
+    @patch('pulzarutils.utils.Utils.get_current_datetime')
     @patch('pulzarcore.core_db.DB.get_keys_values')
     @patch.object(NodeUtils, '_node_candidates_since_path')
-    def test_pick_a_volume2(self, mock_volumes, mock_db):
-        mock_volumes.return_value = [b'node_a']
+    def test_pick_a_volume2(self, mock_volumes, mock_db, utils):
+        utils.return_value = datetime.datetime.strptime('2020-01-01-00-00-00', '%Y-%m-%d-%H-%M-%S')
+        mock_volumes.return_value = ['node_a']
         mock_db.return_value = [('node_a', b'10:10:10:2020-01-01-00-00-00')]
-        print(self.node_utils.pick_a_volume2('/example/fibbonacci'))
+        self.assertEqual(
+            'node_a',
+            self.node_utils.pick_a_volume2('/example/fibbonacci')
+        )
+
+    @patch('pulzarutils.utils.Utils.get_current_datetime')
+    @patch('pulzarcore.core_db.DB.get_keys_values')
+    @patch.object(NodeUtils, '_node_candidates_since_path')
+    def test_pick_a_volume2_different_volumes(self, mock_volumes, mock_db, utils):
+        utils.return_value = datetime.datetime.strptime('2020-01-01-00-00-00', '%Y-%m-%d-%H-%M-%S')
+        mock_volumes.return_value = ['node_a', 'node_b', 'node_c']
+        mock_db.return_value = [
+            ('node_a', b'10:10:10:2020-01-01-00-00-00'),
+            ('node_b', b'12:10:10:2020-01-01-00-00-00'),
+            ('node_c', b'9:10:10:2020-01-01-00-00-00'),
+        ]
+        self.assertEqual(
+            'node_c',
+            self.node_utils.pick_a_volume2('/example/fibbonacci')
+        )
+
+
     
 
 if __name__ == '__main__':

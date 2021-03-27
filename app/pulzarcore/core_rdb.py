2f2d943f75255ae925326127730312b3ab48ceca
Mauricio <mauricio.cleveland@gmail.com>
2021-03-27 18:27:50 -0300

    Added docstring

--- app/pulzarcore/core_rdb.py
+++ app/pulzarcore/core_rdb.py
@@ -1,4 +1,5 @@
 import sqlite3
+from typing import Final
 
 
 class RDB:
@@ -28,7 +29,18 @@ class RDB:
 
     def execute_sql_insert(self, query, param):
         """Insert data
-            return ID (int)
+
+        Parameters
+        ----------
+        query : str
+            query string to be exectuted
+        params : tuple
+            parameters
+        
+        Return
+        ------
+        int
+            Last row inserted
         """
         self.cursor.execute(query, param)
         self.database.commit()
@@ -36,11 +48,44 @@ class RDB:
 
     def execute_sql_update(self, query, param):
         """Update data
-            return rows affected
+
+        Parameters
+        ----------
+        query : str
+            Query to be executed with placeholder
+        param : tuple
+            Tuple with de values specified in the query
+        
+        Return
+        ------
+        int
+            Rows affected
         """
         self.cursor.execute(query, param)
         self.database.commit()
         return self.cursor.rowcount
+
+    def secuential_executions(self, executions):
+        """Multiple executions
+
+        Parameters
+        ----------
+        executions : iterator
+            string iterator
+        
+        Return
+        ------
+        bool
+            True if the operations were correct
+        """
+        try:
+            for sql in executions:
+                self.cursor.execute(sql)
+            
+            self.database.commit()
+            return True
+        except BaseException as err:
+            return False
 
     def close(self):
         """Close conn

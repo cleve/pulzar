081b44b6c4b7aa7dce9b8b0f37ac3a153eeb3bca
Mauricio <mauricio.cleveland@gmail.com>
2021-03-27 18:21:24 -0300

    Added updates to node

--- app/pulzarutils/dbupdater.py
+++ app/pulzarutils/dbupdater.py
@@ -2,12 +2,46 @@ import sys
 from pulzarutils.constants import Constants
 from pulzarcore.core_rdb import RDB
 
+
 def update_schema(schema_type):
     """Compare version and run the update
     """
+    def check_metadata(iterator):
+        for item in iterator:
+            if item[0] == 'metadata':
+                return True
+        return False    
     db_path = Constants.DB_JOBS if schema_type == 'master' else Constants.DB_NODE_JOBS
     rdb = RDB(db_path)  
     current_version = Constants.VERSION
+    result = rdb.execute_sql_with_results('SELECT name FROM sqlite_master WHERE type="table"')
+    if not check_metadata(result):
+        rdb.execute_sql(""
+            """
+            CREATE TABLE "metadata" (
+                "id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+                "version"	TEXT NOT NULL UNIQUE,
+                "sqlcmd"	TEXT
+            )
+            """
+        )
+        rdb.execute_sql_insert('INSERT INTO metadata (version) VALUES(?)', (current_version,))
+        # Only for master
+        if schema_type == 'volume':
+            return
+        sql = """
+        CREATE TABLE "job_catalog" (
+            "id"	INTEGER NOT NULL,
+            "path"	TEXT NOT NULL UNIQUE,
+            "description"	TEXT,
+            "args"	TEXT,
+            "category"	TEXT,
+            "author"	TEXT,
+            PRIMARY KEY("id" AUTOINCREMENT)
+        )
+        """
+        rdb.execute_sql(sql)
+
     result = rdb.execute_sql_with_results('SELECT version, sqlcmd FROM metadata ORDER BY id DESC LIMIT 1')
     if len(result) != 1:
         raise ('Metadata should have len=1')
@@ -27,7 +61,8 @@ if __name__ == "__main__":
     args = sys.argv
     if len(args) == 2 and isinstance(args[1], str):
         update_schema(args[1])
-    raise ('Argument error')
+    else:
+        raise ('Argument error')
 
 
 

b7d360173cf285f1cc6ac07f90b0ca2d7c713901
Mauricio <mauricio.cleveland@gmail.com>
2021-03-29 10:38:54 -0300

    Using decorators

--- app/pulzarutils/dbupdater.py
+++ app/pulzarutils/dbupdater.py
@@ -2,51 +2,35 @@ import sys
 from pulzarutils.constants import Constants
 from pulzarcore.core_rdb import RDB
 
+def is_master(func):
+    def wrapper(iterator, rdb, schema_type):
+        if schema_type == 'master':
+            return func(iterator, rdb)
+        else:
+            return False
+    return wrapper
 
-def update_schema(schema_type):
-    """Compare version and run the update
-    """
-    def check_node_catalog(iterator):
-        for item in iterator:
-            if item[0] == 'job_catalog_node_register':
-                return True
-        return False 
-
-    def check_jobcatalog(iterator):
-        for item in iterator:
-            if item[0] == 'job_catalog':
-                return True
-        return False
-
-    def check_metadata(iterator):
-        for item in iterator:
-            if item[0] == 'metadata':
-                return True
-        return False    
-    db_path = Constants.DB_JOBS if schema_type == 'master' else Constants.DB_NODE_JOBS
-    rdb = RDB(db_path)  
-    current_version = Constants.VERSION
-    result = rdb.execute_sql_with_results('SELECT name FROM sqlite_master WHERE type="table"')
-    if not check_metadata(result):
-        rdb.execute_sql(""
-            """
-            CREATE TABLE "metadata" (
-                "id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-                "version"	TEXT NOT NULL UNIQUE,
-                "sqlcmd"	TEXT
-            )
-            """
-        )
-        rdb.execute_sql_insert('INSERT INTO metadata (version) VALUES(?)', (current_version,))
-        # Only for master
-        if schema_type == 'volume':
+@is_master
+def check_node_catalog(iterator, rdb):
+    for item in iterator:
+        if item[0] == 'job_catalog_node_register':
             return
-        result = rdb.execute_sql_with_results('SELECT name FROM sqlite_master WHERE type="table"')
-        # Job catalog is only available in new versions
-        if check_jobcatalog(result):
+    
+    sql = """
+    CREATE TABLE "job_catalog_node_register" (
+        "id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        "node"	TEXT NOT NULL,
+        "job_catalog_id"	INTEGER NOT NULL,
+        FOREIGN KEY("job_catalog_id") REFERENCES "job_catalog"("id")
+    )"""
+    rdb.execute_sql(sql)
+
+@is_master
+def check_jobcatalog(iterator, rdb):
+    for item in iterator:
+        if item[0] == 'job_catalog':
             return
-        
-        sql = """
+    sql = """
         CREATE TABLE "job_catalog" (
             "id"	INTEGER NOT NULL,
             "path"	TEXT NOT NULL UNIQUE,
@@ -57,19 +41,42 @@ def update_schema(schema_type):
             PRIMARY KEY("id" AUTOINCREMENT)
         )
         """
-        rdb.execute_sql(sql)
+    rdb.execute_sql(sql)
 
-        if check_node_catalog(result):
+def check_metadata(iterator, current_version, rdb):
+    for item in iterator:
+        if item[0] == 'metadata':
             return
+    rdb.execute_sql(""
+            """
+            CREATE TABLE "metadata" (
+                "id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+                "version"	TEXT NOT NULL UNIQUE,
+                "sqlcmd"	TEXT
+            )
+            """
+        )
+    rdb.execute_sql_insert('INSERT INTO metadata (version) VALUES(?)', (current_version,))    
 
-        sql = """
-        CREATE TABLE "job_catalog_node_register" (
-            "id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            "node"	TEXT NOT NULL,
-            "job_catalog_id"	INTEGER NOT NULL,
-            FOREIGN KEY("job_catalog_id") REFERENCES "job_catalog"("id")
-        )"""
-        rdb.execute_sql(sql)
+updates = {
+    'update_node_catalog': check_node_catalog,
+    'update_job_catalog': check_jobcatalog,
+    'update_metadata': check_metadata
+}
+
+def update_schema(schema_type):
+    """Compare version and run the update
+    """
+    db_path = Constants.DB_JOBS if schema_type == 'master' else Constants.DB_NODE_JOBS
+    rdb = RDB(db_path)  
+    current_version = Constants.VERSION
+    result = rdb.execute_sql_with_results('SELECT name FROM sqlite_master WHERE type="table"')
+    
+    # Tables creation
+    updates.get('update_metadata')(result, current_version, rdb)
+    updates.get('update_job_catalog')(result, rdb, schema_type)
+    updates.get('update_node_catalog')(result, rdb, schema_type)
+    
 
     result = rdb.execute_sql_with_results('SELECT version, sqlcmd FROM metadata ORDER BY id DESC LIMIT 1')
     if len(result) != 1:
